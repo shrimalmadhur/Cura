a550e9f3f2a34cea66c81ec688d1f0c66e73f129
Cura <sumanth1117@yahoo.co.in>
2015-04-09 17:54:30 -0400

    Added the rest framework app to the settings

--- infra_server/infra_server/settings.py
+++ infra_server/infra_server/settings.py
@@ -34,6 +34,8 @@ INSTALLED_APPS = (
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
+    'rest_framework',
+    'rest_api',
     'django.contrib.messages',
     'django.contrib.staticfiles',
 )
