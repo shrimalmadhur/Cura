d90ecaad5be72d2b219693d4d0a120a1f879616f
aaqibhabib <aaqib.habib@gmail.com>
2015-04-22 19:03:15 -0400

    added switch API support, CORS issue, use --disable-security flag

--- cura/www/js/controllers.js
+++ cura/www/js/controllers.js
@@ -6,42 +6,42 @@ angular.module('starter.controllers', ['ngCordova','nvd3'])
   $ionicModal.fromTemplateUrl('contact-modal.html', {
     scope: $scope,
     animation: 'slide-in-up'
-  }).then(function(modal) {
+}).then(function(modal) {
     $scope.modal = modal
-  })  
+})  
 
 
-  $scope.openModal = function() {
+$scope.openModal = function() {
     $scope.modal.show()
-  }
+}
 
-  $scope.closeModal = function() {
+$scope.closeModal = function() {
     $scope.modal.hide();
-  };
+};
 
-  $scope.$on('$destroy', function() {
+$scope.$on('$destroy', function() {
     $scope.modal.remove();
-  });
+});
 
-  $scope.doAlarm = function(){
-      $scope.alarmInterval = setInterval(function(){ 
-        $cordovaVibration.vibrate(100) 
-      }, 1000);
+$scope.doAlarm = function(){
+  $scope.alarmInterval = setInterval(function(){ 
+    $cordovaVibration.vibrate(100) 
+}, 1000);
 
-      $scope.openModal();
-  }
+  $scope.openModal();
+}
 
-  $scope.stopAlarm = function(){
+$scope.stopAlarm = function(){
     $scope.closeModal();
     clearInterval($scope.alarmInterval);
-  }
+}
 })
 
 .controller('ChatsCtrl', function($scope, Chats) {
   $scope.chats = Chats.all();
   $scope.remove = function(chat) {
     Chats.remove(chat);
-  }
+}
 })
 
 .controller('ChatDetailCtrl', function($scope, $stateParams, Chats) {
@@ -54,8 +54,8 @@ angular.module('starter.controllers', ['ngCordova','nvd3'])
     $cordovaContacts.pickContact().then(function (contact){
       alert(JSON.stringify(contact))      
       $scope.friends.push(contact);
-    })
-  }
+  })
+}
 })
 
 .controller('FriendDetailCtrl', function($scope, $stateParams, Friends) {
@@ -65,59 +65,59 @@ angular.module('starter.controllers', ['ngCordova','nvd3'])
 .controller('VisualCtrl', function($scope, $stateParams, Visuals) {
   $scope.visuals = [{values:Visuals.all(), key:'Test Wave', color: '#ff7f0e'}];
   $scope.vOption = {
-            chart: {
-                type: 'lineChart',
-                height: 450,
-                margin : {
-                    top: 20,
-                    right: 20,
-                    bottom: 40,
-                    left: 55
-                },
-                x: function(d){ return d.x; },
-                y: function(d){ return d.y; },
-                useInteractiveGuideline: true,
-                dispatch: {
-                    stateChange: function(e){ console.log("stateChange"); },
-                    changeState: function(e){ console.log("changeState"); },
-                    tooltipShow: function(e){ console.log("tooltipShow"); },
-                    tooltipHide: function(e){ console.log("tooltipHide"); }
-                },
-                xAxis: {
-                    axisLabel: 'Time (ms)'
-                },
-                yAxis: {
-                    axisLabel: 'Voltage (v)',
-                    tickFormat: function(d){
-                        return d3.format('.02f')(d);
-                    },
-                    axisLabelDistance: 30
-                },
-                callback: function(chart){
-                    console.log("!!! lineChart callback !!!");
-                }
+    chart: {
+        type: 'lineChart',
+        height: 450,
+        margin : {
+            top: 20,
+            right: 20,
+            bottom: 40,
+            left: 55
+        },
+        x: function(d){ return d.x; },
+        y: function(d){ return d.y; },
+        useInteractiveGuideline: true,
+        dispatch: {
+            stateChange: function(e){ console.log("stateChange"); },
+            changeState: function(e){ console.log("changeState"); },
+            tooltipShow: function(e){ console.log("tooltipShow"); },
+            tooltipHide: function(e){ console.log("tooltipHide"); }
+        },
+        xAxis: {
+            axisLabel: 'Time (ms)'
+        },
+        yAxis: {
+            axisLabel: 'Voltage (v)',
+            tickFormat: function(d){
+                return d3.format('.02f')(d);
             },
-            title: {
-                enable: true,
-                text: 'Line Chart Sample 1'
-            },
-            subtitle: {
-                enable: true,
-                text: 'Subtitle for simple line chart. Lorem ipsum dolor sit amet, at eam blandit sadipscing, vim adhuc sanctus disputando ex, cu usu affert alienum urbanitas.',
-                css: {
-                    'text-align': 'center',
-                    'margin': '10px 13px 0px 7px'
-                }
-            },
-            caption: {
-                enable: true,
-                html: '<b>Figure 1.</b> Lorem ipsum dolor sit amet, at eam blandit sadipscing, <span style="text-decoration: underline;">vim adhuc sanctus disputando ex</span>, cu usu affert alienum urbanitas. <i>Cum in purto erat, mea ne nominavi persecuti reformidans.</i> Docendi blandit abhorreant ea has, minim tantas alterum pro eu. <span style="color: darkred;">Exerci graeci ad vix, elit tacimates ea duo</span>. Id mel eruditi fuisset. Stet vidit patrioque in pro, eum ex veri verterem abhorreant, id unum oportere intellegam nec<sup>',
-                css: {
-                    'text-align': 'justify',
-                    'margin': '10px 13px 0px 7px'
-                }
-            }
-        };
+            axisLabelDistance: 30
+        },
+        callback: function(chart){
+            console.log("!!! lineChart callback !!!");
+        }
+    },
+    title: {
+        enable: true,
+        text: 'Line Chart Sample 1'
+    },
+    subtitle: {
+        enable: true,
+        text: 'Subtitle for simple line chart. Lorem ipsum dolor sit amet, at eam blandit sadipscing, vim adhuc sanctus disputando ex, cu usu affert alienum urbanitas.',
+        css: {
+            'text-align': 'center',
+            'margin': '10px 13px 0px 7px'
+        }
+    },
+    caption: {
+        enable: true,
+        html: '<b>Figure 1.</b> Lorem ipsum dolor sit amet, at eam blandit sadipscing, <span style="text-decoration: underline;">vim adhuc sanctus disputando ex</span>, cu usu affert alienum urbanitas. <i>Cum in purto erat, mea ne nominavi persecuti reformidans.</i> Docendi blandit abhorreant ea has, minim tantas alterum pro eu. <span style="color: darkred;">Exerci graeci ad vix, elit tacimates ea duo</span>. Id mel eruditi fuisset. Stet vidit patrioque in pro, eum ex veri verterem abhorreant, id unum oportere intellegam nec<sup>',
+        css: {
+            'text-align': 'justify',
+            'margin': '10px 13px 0px 7px'
+        }
+    }
+};
 })
 
 .controller('MedicationsCtrl', function($scope, $stateParams, $ionicModal, $cordovaLocalNotification, Medications){
@@ -126,26 +126,26 @@ angular.module('starter.controllers', ['ngCordova','nvd3'])
   $ionicModal.fromTemplateUrl('templates/medication-add-modal.html', {
     scope: $scope,
     animation: 'slide-in-up'
-  }).then(function(modal) {
+}).then(function(modal) {
     $scope.modal = modal
-  })  
+})  
 
-  $scope.newMedication = {};
+$scope.newMedication = {};
 
 
-  $scope.openModal = function() {
+$scope.openModal = function() {
     $scope.modal.show()
-  }
+}
 
-  $scope.closeModal = function() {
+$scope.closeModal = function() {
     $scope.modal.hide();
-  };
+};
 
-  $scope.$on('$destroy', function() {
+$scope.$on('$destroy', function() {
     $scope.modal.remove();
-  });
+});
 
-  $scope.update = function(newMedication) {
+$scope.update = function(newMedication) {
 
     var newId = $scope.medications.length;
     var newMed = angular.copy(newMedication);
@@ -174,11 +174,11 @@ angular.module('starter.controllers', ['ngCordova','nvd3'])
 
 
 
-  };
+};
 
-  $scope.reset = function() {
+$scope.reset = function() {
     $scope.user = angular.copy({});
-  };
+};
 
 
 
@@ -194,55 +194,45 @@ angular.module('starter.controllers', ['ngCordova','nvd3'])
 })
 
 .controller('HomeCtrl', function($scope, $http, $timeout) {
-  var url = "http://128.2.83.208:8006/api/v1/homeautomation/ha_user/";
-  // init
-  $http.get(url).success(function(data){
-    //update settings with data
-  });
+    var url = "http://128.2.83.208:8001/api/v1/homeautomation/ha_user/";
+    $scope.switches = [];
+    $scope.thermostat;
 
-  $scope.settings = {
-    tvSwitch: true,
-    isHeating: true,
-    temprature: 70,
-    mode: function(){
-      return $scope.settings.isHeating ? "Heating" : "Cooling";
-    }
-  };
-
-  // TV Swtich, 1155B6
-  $scope.$watch('settings.tvSwitch', function() {
-    console.log('TV changed:' + $scope.settings.tvSwitch);
-    $http.put(url, JSON.stringify({
-      user_name: "ha_user",
-      tag_id: "1155B6",
-      signal_type: "swtich",
-      current_value: 0,
-      required_value: $scope.settings.tvSwitch ? 0 : 1
-    }))
-  });
-
-  //temprature
-  var timeoutId = null;
-  $scope.$watch('settings.temprature', function() {
-    //console.log('Has changed');
-        if(timeoutId !== null) {
-            //console.log('Ignoring this movement');
-            return;
+    $http.get(url).success(function(devices){
+        for (id in devices){
+            var device = devices[id];
+            if (device.signal_type === "switch"){
+                $scope.switches.push({ name: device.tag_id, isOn: device.required_value === "1" ? true : false});
+            } else if (device.signal_type === "thermo"){
+                var temp = parseInt(device.required_value, 16)/2;
+                $scope.thermostat = {name: device.tag_id, temp: temp, isHeating: device.mode === "hot" ? true : false};
+            }
         }
-        //console.log('Not going to ignore this one');
-        timeoutId = $timeout( function() {
-            console.log('It changed recently!');
-            $timeout.cancel(timeoutId);
-            timeoutId = null;
-            // Now load data from server, check url and params
-            url = "/api/v1/home_automation";
-            data = {
-              user_name: 'ha_user',
-              tag_id: '111',
-              current_value: 0,
-              required_value: 1
-          }
-            //$http.put(url, JSON.stringify(data));
-        }, 1000); 
-  });
+    });
+
+    // Temp String Format
+    $scope.tempMode = function(){
+        if($scope.thermostat){
+            return $scope.thermostat.isHeating?"Heating":"Cooling";
+        }
+        return "";
+    }
+    $scope.switchToggle = function(switchData){
+/*        console.log(JSON.stringify({
+            user_name: "ha_user",
+            tag_id: switchData.name,
+            signal_type: "switch",
+            required_value: "1",
+            current_value: "0",
+            mode:null
+        }));*/
+        $http.put(url, {
+            user_name:"ha_user",
+            tag_id:switchData.name,
+            signal_type:"switch",
+            current_value:"0",
+            required_value: switchData.isOn ? "1": "0",
+            mode:"null"
+        });
+    }
 });
