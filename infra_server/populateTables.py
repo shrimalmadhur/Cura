37b98ab90a24f2d86d4f62fe94d9773f77dfbd00
Cura <sumanth1117@yahoo.co.in>
2015-04-29 01:50:54 -0400

    Updated the requirements with the JSONp and added the DB population script

---
+++ infra_server/populateTables.py
@@ -0,0 +1,40 @@
+import os
+import datetime
+from rest_api.models import ( Biometrics, BiometricsPrecise )
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "infra_server.settings")
+
+
+def populate_bloodpressure(user_name,
+                           time_recorded,
+                           sample_points,
+
+
+                           
+):
+
+def populate_biometrics(user_name, 
+                        time_recorded, 
+                        sample_points, 
+                        heart_rate = 50,
+                        breathing_rate = '50',
+                        ecg = '50',
+                        estimated_core_temperature = '50',
+                        posture = '50'):
+  for i in xrange(0, sample_points):
+    new_biometric = Biometrics(user_name = user_name,
+                               time_recorded = time_recorded,
+                               heart_rate = heart_rate,
+                               breathing_rate = breathing_rate,
+                               ecg = ecg,
+                               estimated_core_temperature = estimated_core_temperature,
+                               posture = posture)
+    new_biometric.save()
+
+def populate_sleep(user_name,
+                  )
+
+if __name__ == '__main__':
+  populate_biometrics(user_name = 'moko', 
+                      time_recorded = datetime.date(2015, 4, 12), 
+                      sample_points = 2)
